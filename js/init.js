24570624f4d4d2c37ef3c036dd80f926b53ff97b
nerain <nerain.ux@outlook.com>
2024-05-24 20:41:43 +0400

    making dots click for the slider

--- js/init.js
+++ js/init.js
@@ -7,10 +7,40 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let sliderWrapper = document.querySelectorAll('.slider_ .slider_wrapper');
     let sliderWrapperLength = sliderWrapper.length;
     let totalWidthOfSlider = sliderWrapperLength*mainCarouselWidth;
+    let dots_wrapper = document.querySelector('.dots_wrapper');
     
     
     sliderHolder.style.width = `${totalWidthOfSlider}px`;
     
+
+
+    // creating the dots button
+    for(i=0; i < sliderWrapperLength; i++){
+        let buttonWrap = document.createElement("div");
+        buttonWrap.className = "button";
+        dots_wrapper.append(buttonWrap);
+    }
+
+    let buttons = document.querySelectorAll('.button');
+    buttons[0].style.backgroundColor = "blue";
+    const resetDotsBg = () =>{
+        buttons.forEach((button)=>{
+            button.style.backgroundColor = "transparent";
+        });
+    }
+    buttons.forEach((button, i)=>{
+        button.addEventListener('click', ()=>{
+            resetDotsBg();
+            sliderHolder.style.transform = `translateX(-${i*mainCarouselWidth}px)`;
+            sliderNumber = i;
+            button.style.backgroundColor = "blue";
+        })
+    })
+
+    const changeDotsColor = () =>{
+        resetDotsBg();
+        buttons[sliderNumber-1].style.backgroundColor = "blue";
+    }
     // giving dynamic width value to each slider image holder div
     sliderWrapper.forEach(wrapper=>{
         wrapper.style.width = `${totalWidthOfSlider / sliderWrapperLength}px`;
@@ -26,29 +56,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
         if (sliderNumber < sliderWrapperLength) {
             updateSlidePosition(sliderNumber * mainCarouselWidth);
             sliderNumber++;
+            changeDotsColor();
         } else {
             firstSlide();
+            changeDotsColor();
         }
-        console.log('sliderNumber', sliderNumber);
-        console.log('nextSlideNum', `translateX(-${sliderNumber*mainCarouselWidth}px)`);
+        // console.log('sliderNumber', sliderNumber);
+        // console.log('nextSlideNum', `translateX(-${sliderNumber*mainCarouselWidth}px)`);
     }
     
     // this function is the prev slide
     
     const prevSlide = () =>{
         if(sliderNumber > 1){
-            sliderHolder.style.transform = `translateX(-${(sliderNumber-2)*mainCarouselWidth}px)`;
+            updateSlidePosition((sliderNumber-2)*mainCarouselWidth);
             sliderNumber--;
+            changeDotsColor();
         }else{
             lastSlide();
+            changeDotsColor();
         }
         
-        console.log('prevSlideNum', sliderNumber);
+        // console.log('prevSlideNum', sliderNumber);
     }
     const firstSlide = () => {
         setTimeout(() => {
             updateSlidePosition(0);
             sliderNumber = 1;
+            changeDotsColor();
         }, 100); // Adjust delay as necessary to match your transition duration
     };
     const lastSlide = () => {
@@ -56,6 +91,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
         sliderNumber = sliderWrapperLength;
     }
     
-    rightSlide.addEventListener('click', nextSlide);
-    leftSlide.addEventListener('click', prevSlide);
-})
+    rightSlide.addEventListener('click', ()=>{
+        nextSlide();
+    });
+    leftSlide.addEventListener('click', ()=>{
+        prevSlide();
+    });
+
+
+});
